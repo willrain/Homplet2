bd00314a5fecf5e939bdb97243864cf37a36872b
willrain <willrain@emart.com>
2017-05-27 15:50:55 +0900

    setting

---
+++ common-library/src/main/java/com/willrain/framework/CommonLibraryApplication.java
@@ -0,0 +1,12 @@
+package com.willrain.framework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommonLibraryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CommonLibraryApplication.class, args);
+	}
+}
